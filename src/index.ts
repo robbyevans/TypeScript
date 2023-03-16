e615f7b252e042cc30b914f7e625c446f0617b20
Evans Rutto <robbyevans001@gmail.com>
2023-03-16 21:46:02 +0300

    first initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+console.log("hello world")
+
+let age:number=20;
+
+if(age<50){
+  age+=10;
+}
+console.log(age)
+
+function handleClick(document:string){
+  console.log(document)
+}
+
+//arrays
+
+let numbers:number[] =[1,2,3,4,5]
+
+let words:string[]=["hello","mandy","jean"]
+
+
+//tuples
+
+let array:[number,string]=[1,"robby"]
+
