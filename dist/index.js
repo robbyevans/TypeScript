1971fa5a09668bc84e4c9ec2c61b960755693540
Evans Rutto <robbyevans001@gmail.com>
2023-03-18 10:05:37 +0300

    progress

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,68 @@
 "use strict";
-console.log("hello world");
 let age = 20;
 if (age < 50) {
     age += 10;
 }
 console.log(age);
+function handleClick(document) {
+    if (typeof document === "string") {
+        return document;
+    }
+    else {
+        return ("undefined");
+    }
+}
+let numbers = [1, 2, 3, 4, 5];
+let words = ["hello", "mandy", "jean"];
+let array = [1, "robby"];
+let mySize = 2;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2023) {
+        return income * 0.16;
+    }
+    return income * 0;
+}
+console.log(calculateTax(100000, 2025));
+let employee = {
+    id: 1,
+    name: "Robby",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgsToLbs(weight) {
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+console.log(kgsToLbs("10kgs"));
+let textBox = {
+    drag: () => {
+    },
+    resize: () => {
+    },
+};
+let quantity = 50;
+let units = 'cm';
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else {
+        console.log("Holla Papi!");
+    }
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+if (customer !== null && customer !== undefined)
+    console.log(customer.birthday);
+let newCustomer = getCustomer(0);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-console.log("hello world")
+//INTRODUCTION TO TYPESCRIPT
 
+
+//:anotations
 let age:number=20;
 
 if(age<50){
-  age+=10;
+  age+=10;  
 }
 console.log(age)
 
-function handleClick(document:string){
-  console.log(document)
+//functions
+
+function handleClick(document:string):string{
+  if(typeof document==="string"){
+    return document
+  }
+  else{
+    return ("undefined")
+  }
 }
 
 //arrays
@@ -18,7 +27,130 @@ let numbers:number[] =[1,2,3,4,5]
 let words:string[]=["hello","mandy","jean"]
 
 
-//tuples
+//tuples (its an array containing both numbers and strings)
 
 let array:[number,string]=[1,"robby"]
 
+//enum
+
+const enum Size{Small=1,Medium,Large}
+
+let mySize:Size = Size.Medium;
+
+console.log(mySize);
+
+
+//functions
+
+function calculateTax(income:number,taxYear=2022):number{
+  
+  if(taxYear<2023){
+    return income*0.16; 
+  }
+  return income*0 
+}
+console.log(calculateTax(100_000,2025))
+
+
+//OBJECTS
+
+type Employee={
+  readonly id:number,
+  name:string,
+  retire:(date:Date)=>void
+}
+
+let employee:Employee={
+  id:1,
+  name:"Robby",
+  retire:(date:Date)=>{
+    console.log(date)
+  }
+}
+
+
+//union types
+
+function kgsToLbs(weight:number|string):number{
+  if(typeof weight==="number"){
+    return weight*2.2
+  }
+  else{
+    return parseInt(weight)*2.2
+  }
+}
+
+console.log(kgsToLbs("10kgs"))
+
+//Intersection types
+
+type Dragable={
+  drag:()=>void
+};
+
+type Resizeable={
+  resize:()=>void
+};
+
+type UIWidget=Dragable&Resizeable
+
+let textBox:UIWidget={
+  drag:() =>{
+    
+  },
+  resize:()=>{
+     
+  },
+}
+
+
+//literal types(can only contain specific values )
+
+//example
+
+type Quantity=50|100;
+let quantity:Quantity=50;
+
+
+
+type Metric='cm'|'inch';
+let units:Metric='cm';
+
+//Nullable types (to allow our functions take up null or undefined in absence of the 
+//value without raising an error)
+
+function greet(name:string|null|undefined){
+  if(name){
+    console.log(name.toUpperCase())
+  }
+  else{
+    console.log("Holla Papi!")
+  }
+}
+greet(null)
+
+//optional chaining
+
+type Customer={
+  birthday:Date
+};
+
+
+function getCustomer(id:number):Customer |null|undefined{
+  return id===0?null:{birthday:new Date()}
+}
+
+let customer=getCustomer(0);
+  if(customer!==null && customer!==undefined)
+    console.log(customer.birthday)
+
+
+    //we can use the optional property access operator to replace the if statement
+    //above i.e
+
+    let newCustomer=getCustomer(0);
+    //optional property access operator
+      console.log(customer?.birthday)
+
+
+      //this code above means the code is executed if we have a customer that neither null or undefined
